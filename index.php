bc3543d39ba13c786603b730e37a8f1d91b145d5
James Pow <jbpow18@icloud.com>
2023-02-04 16:34:15 -0500

    Included views.

--- index.php
+++ index.php
@@ -25,39 +25,11 @@
 </head>
 
 <body>
-  <header>
-    <h1>Explore the world</h1>
-  </header>
-  <main>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
-      <input type="text" name="country" placeholder="Enter a country">
-      <select name="category">
-        <option value="Continent">Continent</option>
-        <option value="Region">Region</option>
-        <option value="SurfaceArea">Surface Area</option>
-        <option value="IndepYear">Year of Independence</option>
-        <option value="Population">Population</option>
-        <option value="LocalName">Local Name</option>
-        <option value="HeadOfState">Head Of State</option>
-        <option value="Capital">Capital</option>
-      </select>
-      <input type="submit" value="Submit">
-      <input type="button" value="Clear" onclick="window.location.href='index.php'">
-    </form>
-    <div class="result">
-      
-      <?php if(isset($country) && isset($category)) {
-          echo "<h3>{$country}</h3>";
-          echo "<p>{$category}: {$results[0][$category]}</p>";
-        }
-        
-      ?>
-    </div>
-  </main>
-  <footer>
-    <p>Contact us at contact@exploretheworld.com</p>
-    <p>Copyright &copy; 2023 Explore the world</p>
-  </footer>
+  <?php include("./view/header.php"); ?>
+
+  <?php include("./view/exploreMain.php"); ?>
+
+  <?php include("./view/footer.php"); ?>
 </body>
 
 </html>
