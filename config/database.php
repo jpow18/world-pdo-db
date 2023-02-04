1a6c0d04fdc767791abfd08826ec58c9aa624a49
James Pow <jbpow18@icloud.com>
2023-02-04 14:53:31 -0500

    Configured PDO instance.

---
+++ config/database.php
@@ -0,0 +1,23 @@
+<?php 
+
+  $host = 'localhost';
+  $user = 'root';
+  $pWord = 'sdmV[TQqecJ@8_u4';
+  $dbname = 'world';
+
+  // Set DSN (data source name)
+  $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+
+  try {
+    // Create PDO instance
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  } catch (PDOException $e) {
+    $error_message = 'Database Error: ';
+    $error_message .= $e->getMessage();
+    echo $error_message;
+    exit('Unable to connect to the database');
+  }
+
+?>
